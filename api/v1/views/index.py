b4e386b9ce65e88152da15c8e8bddbe0abd494d2
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-30 23:14:31 +0100

    feat: add an endpoint to serve stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,23 @@
 for app views
 """
 from api.v1.views import app_views
+from models import storage, classes
 
 
 @app_views.route("/status")
 def status():
     return {"status": "OK"}
+
+
+@app_views.route("/stats")
+def stats():
+    payload = {}
+    clss = {"amenities": classes["Amenity"], "cities": classes["City"],
+            "places": classes["Place"], "reviews": classes["Review"],
+            "states": classes["State"], "users": classes["User"]}
+
+    for name, cls in clss.items():
+        if cls.__name__ != "BaseModel":
+            payload[name] = storage.count(cls)
+
+    return payload
