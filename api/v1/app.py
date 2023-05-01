06d14b2e9f5d79ebe54731971b96a440f5013353
Awe Ayomidipupo <pupoawe@gmail.com>
2023-05-01 05:54:14 +0100

    feat: implement CORS for flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ import os
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app=app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
